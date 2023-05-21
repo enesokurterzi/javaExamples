c88ee965875f5cef7a2664b62398c414f0148520
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2023-05-21 19:03:05 +0300

    Delete PartFour/Devs directory

--- PartFour/Devs/src/main/java/Kodlama/io/Devs/dataAccess/abstracts/LanguageRepository.java
+++
@@ -1,9 +0,0 @@
-package Kodlama.io.Devs.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Kodlama.io.Devs.entities.concretes.Language;
-
-public interface LanguageRepository extends JpaRepository<Language, Integer> {
-	
-}
